a766877839defc2ab1c5bb98bd2e0982f4da1b98
Herramientasdocentes <herramientasanluis@gmail.com>
2025-07-28 19:56:37 -0400

    SYNC: Sincronización completa de todos los archivos del proyecto

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Question = require('../models/Question');
-const auth = require('../../middleware/auth');
+const auth = require('../middleware');
 
 
 // @route   GET /api/questions
