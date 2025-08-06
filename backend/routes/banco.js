3a636bc158601a412ee29c0d570fdf4defe6b66f
Usuario herramientas <Herramientasanluis@gmail.com>
2025-08-06 17:48:47 -0400

    Fix-duplicado-express

--- backend/routes/banco.js
+++ backend/routes/banco.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
-const express = require('express');
-const router = express.Router();
 const auth = require('../middleware/auth');
 const Question = require('../models/Question');
 
